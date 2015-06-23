d3279ccaaaca08bb103b0e58e5576808dbf24d6e
Juan Uribe <juribe@isf.com.co>
2015-06-23 10:45:00 -0500

    Automatically generated files

---
+++ Example/Pods/Target Support Files/Pods-UIScrollView-Keyboard_Example-UIScrollView-Keyboard/Pods-UIScrollView-Keyboard_Example-UIScrollView-Keyboard-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double UIScrollView_KeyboardVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIScrollView_KeyboardVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-UIScrollView-Keyboard_Example/Pods-UIScrollView-Keyboard_Example-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// UIScrollView-Keyboard
+#define COCOAPODS_POD_AVAILABLE_UIScrollView_Keyboard
+#define COCOAPODS_VERSION_MAJOR_UIScrollView_Keyboard 0
+#define COCOAPODS_VERSION_MINOR_UIScrollView_Keyboard 1
+#define COCOAPODS_VERSION_PATCH_UIScrollView_Keyboard 0
+
